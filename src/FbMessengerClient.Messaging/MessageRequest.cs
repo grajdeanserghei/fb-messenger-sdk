6a86821267218dbfd9b709027302d282bd64260c
Serghei Grajdean <grajdeanserghei@gmail.com>
2018-09-15 23:34:31 +0300

    Add documentation to MessageRequest and Recippient

--- src/FbMessengerClient.Messaging/MessageRequest.cs
+++ src/FbMessengerClient.Messaging/MessageRequest.cs
@@ -2,13 +2,26 @@ using Newtonsoft.Json;
 
 namespace FbMessengerClient.Messaging
 {
+    /// <summary>
+    /// Root object sent to facebook
+    /// </summary>
     public class MessageRequest
     {
+        /// <summary>
+        /// The messaging type of the message 
+        /// being sent.
+        /// </summary>
         [JsonProperty("messaging_type")]
         public MessagingType? MessagingType {get;set;}
 
+        /// <summary>
+        /// recipient object
+        /// </summary>
         public Recipient Recipient { get; set; }
 
+        /// <summary>
+        /// message object. Cannot be sent with sender_action.
+        /// </summary>
         public IMessage Message { get; set; }
     }
 }
--- src/FbMessengerClient.Messaging/Recipient.cs
+++ src/FbMessengerClient.Messaging/Recipient.cs
@@ -1,7 +1,14 @@
 namespace FbMessengerClient.Messaging
 {
+    /// <summary>
+    /// All requests must include one of id, phone_number, or user_ref.
+    /// </summary>
     public class Recipient
     {
+        /// <summary>
+        /// Either PSID, phone_number, or user_ref 
+        /// of the message recipient.
+        /// </summary>
         public string Id { get; set; }
     }
 }
